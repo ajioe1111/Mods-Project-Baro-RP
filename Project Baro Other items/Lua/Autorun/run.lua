79e014dac8b9b72185039dd118a3daf132a49d3e
ajioe1111 <ajioe1111@gmail.com>
2022-04-22 13:27:30 +0300

    Замени часть старых путей на новые

--- Project Baro Other items/Lua/Autorun/run.lua
+++ Project Baro Other items/Lua/Autorun/run.lua
@@ -1,3 +1,3 @@
-dofile("Mods/Project Baro Other items/Lua/suicide.lua")
-dofile("Mods/Project Baro Other items/Lua/chatlog.lua")
-dofile("Mods/Project Baro Other items/Lua/Nominal_items.lua")
+dofile("%ModDir%/Lua/suicide.lua")
+dofile("%ModDir%/Lua/chatlog.lua")
+dofile("%ModDir%/Lua/Nominal_items.lua")
