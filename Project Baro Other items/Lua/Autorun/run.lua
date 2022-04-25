a999f6ec5d62941da16167d1b0511b5646991c77
ajioe1111 <ajioe1111@gmail.com>
2022-04-26 02:10:52 +0300

    Fix под новую версию

--- Project Baro Other items/Lua/Autorun/run.lua
+++ Project Baro Other items/Lua/Autorun/run.lua
@@ -1,3 +1,3 @@
-dofile("%ModDir%/Lua/suicide.lua")
-dofile("%ModDir%/Lua/chatlog.lua")
-dofile("%ModDir%/Lua/Nominal_items.lua")
+dofile("Mods/Project Baro Other items/Lua/suicide.lua")
+dofile("Mods/Project Baro Other items/Lua/chatlog.lua")
+dofile("Mods/Project Baro Other items/Lua/Nominal_items.lua")
