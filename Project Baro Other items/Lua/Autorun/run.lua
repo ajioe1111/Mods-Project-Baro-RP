9e1ae97f69b1bff7cc4a5ce69c2ed0ce444734fc
h4rtley <caseyh4rtley@gmail.com>
2022-06-13 00:29:05 +0300

    Фикс Луа и Реверб
    Луа: Теперь запускаются скрипты. Заставил вебхук работать.
    Реверб: Требуется проверка, так как особо ничего не заметил.

--- Project Baro Other items/Lua/Autorun/run.lua
+++ Project Baro Other items/Lua/Autorun/run.lua
@@ -1,2 +1,9 @@
-dofile("Mods/Project Baro Other items/Lua/suicide.lua")
-dofile("Mods/Project Baro Other items/Lua/Nominal_items.lua")
+if SERVER then
+    ProjectBaro = {}
+    ProjectBaro.Path = table.pack(...)[1]
+
+	dofile(ProjectBaro.Path .. "/Lua/webhook.lua")
+	dofile(ProjectBaro.Path .. "/Lua/chatlog.lua")
+	dofile(ProjectBaro.Path .. "/Lua/suicide.lua")
+	dofile(ProjectBaro.Path .. "/Lua/Nominal_items.lua")
+end
